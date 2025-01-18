70e5ef64fe270bdf9acf3ac543572f1150248d20
BS0404 <gab0r15@proton.me>
2025-01-18 23:36:43 +0100

    First Commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAC2i3xEJp5z4amTaGslj5JnHNtGUwWLQA",
+
+    authDomain: "beadando20250117.firebaseapp.com",
+
+    projectId: "beadando20250117",
+
+    storageBucket: "beadando20250117.firebasestorage.app",
+
+    messagingSenderId: "337597240220",
+
+    appId: "1:337597240220:web:9195a13b3d7059994f2e59"
+
+  }
+};
